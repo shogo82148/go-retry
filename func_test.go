9cef1f3ac5a683550080cd32d05df77f39ab2176
ICHINOSE Shogo <shogo82148@gmail.com>
2024-06-24 22:19:57 +0900

    add more test cases

--- func_test.go
+++ func_test.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestDoValue_Success(t *testing.T) {
@@ -44,6 +45,22 @@ func TestDoValue_MarkPermanent(t *testing.T) {
 	}
 }
 
+func TestDoValue_WrappedMarkPermanent(t *testing.T) {
+	permanentErr := errors.New("permanent error")
+	policy := &Policy{MaxCount: 10}
+	count := 0
+	_, err := DoValue(context.Background(), policy, func() (int, error) {
+		count++
+		return 0, fmt.Errorf("some error: %w", MarkPermanent(permanentErr))
+	})
+	if err == nil {
+		t.Errorf("want error is %#v, got %#v", err, permanentErr)
+	}
+	if count != 1 {
+		t.Errorf("want %d, got %d", 1, count)
+	}
+}
+
 func TestDoValue_MarkTemporary(t *testing.T) {
 	temporaryErr := errors.New("temporary error")
 	policy := &Policy{MaxCount: 10}
@@ -57,5 +74,19 @@ func TestDoValue_MarkTemporary(t *testing.T) {
 	}
 	if count != 10 {
 		t.Errorf("want %d, got %d", 10, count)
+	}
+}
+
+func TestDoValue_Deadline(t *testing.T) {
+	policy := &Policy{
+		MinDelay: 2 * time.Second,
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	_, err := DoValue(ctx, policy, func() (int, error) {
+		return 0, errors.New("some error")
+	})
+	if err != context.DeadlineExceeded {
+		t.Errorf("want %v, got %v", context.DeadlineExceeded, err)
 	}
 }
