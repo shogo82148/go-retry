7d360124bc5150c56ad2c0495987a82a65c4c9e7
ICHINOSE Shogo <shogo82148@gmail.com>
2024-06-25 20:13:19 +0900

    add tests for wrapped errors

--- retry_test.go
+++ retry_test.go
@@ -281,6 +281,22 @@ func TestDo_MarkPermanent(t *testing.T) {
 	}
 }
 
+func TestDo_MarkPermanent_Wrapped(t *testing.T) {
+	permanentErr := fmt.Errorf("retry: %w", MarkPermanent(errors.New("permanent error")))
+	policy := &Policy{MaxCount: 10}
+	count := 0
+	err := policy.Do(context.Background(), func() error {
+		count++
+		return permanentErr
+	})
+	if err != permanentErr {
+		t.Errorf("want error is %#v, got %#v", err, permanentErr)
+	}
+	if count != 1 {
+		t.Errorf("want %d, got %d", 1, count)
+	}
+}
+
 func TestDo_MarkTemporary(t *testing.T) {
 	temporaryErr := errors.New("temporary error")
 	policy := &Policy{MaxCount: 10}
@@ -288,6 +304,22 @@ func TestDo_MarkTemporary(t *testing.T) {
 	err := policy.Do(context.Background(), func() error {
 		count++
 		return MarkTemporary(temporaryErr)
+	})
+	if err != temporaryErr {
+		t.Errorf("want error is %#v, got %#v", err, temporaryErr)
+	}
+	if count != 10 {
+		t.Errorf("want %d, got %d", 10, count)
+	}
+}
+
+func TestDo_MarkTemporary_Wrapped(t *testing.T) {
+	temporaryErr := fmt.Errorf("retry: %w", MarkTemporary(errors.New("temporary error")))
+	policy := &Policy{MaxCount: 10}
+	count := 0
+	err := policy.Do(context.Background(), func() error {
+		count++
+		return temporaryErr
 	})
 	if err != temporaryErr {
 		t.Errorf("want error is %#v, got %#v", err, temporaryErr)
