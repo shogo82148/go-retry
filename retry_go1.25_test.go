77796bfb6efc40a12c0010e5949164ad161d8b0f
ICHINOSE Shogo <shogo82148@gmail.com>
2025-12-10 19:42:03 +0900

    re-implement TestRetry_WithJitter

--- retry_go1.25_test.go
+++ retry_go1.25_test.go
@@ -65,3 +65,37 @@ func TestRetry_NoMaxDelay(t *testing.T) {
 		}
 	})
 }
+
+func TestRetry_WithJitter(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		want := []time.Duration{
+			// sleepContext is not called as first
+			0,
+
+			// exponential back off
+			time.Second, 2 * time.Second, 4 * time.Second, 8 * time.Second, 16 * time.Second, 32 * time.Second,
+
+			// reach MaxDelay
+			60 * time.Second, 60 * time.Second, 60 * time.Second,
+		}
+		policy := &Policy{
+			MinDelay: time.Second,
+			MaxDelay: time.Minute,
+			Jitter:   time.Second,
+		}
+		retrier := policy.Start(t.Context())
+		for i := range 10 {
+			start := time.Now()
+			if !retrier.Continue() {
+				t.Error("want to continue, but not")
+			}
+			delay := time.Since(start)
+			if delay < want[i] {
+				t.Errorf("want greater than or equal to %s, got %s", want[i], delay)
+			}
+			if delay >= want[i]+time.Second {
+				t.Errorf("want less than %s, got %s", want[i]+policy.Jitter, delay)
+			}
+		}
+	})
+}
